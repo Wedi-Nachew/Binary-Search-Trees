ab3a9f16d32fa4a45b950d309b4d01356dc9f517
Filomn Abrha <filmonabrha2020@gmail.com>
2023-08-22 11:21:26 +0300

    Create a node factory function that returns the value & it's left & right attributes

--- binary-search-tree.js
+++ binary-search-tree.js
@@ -11,3 +11,18 @@ const Node = (value) => {
     let right = null;
     return { value, left, right };
 };
+
+function buildTree(array, start = 0, end = array.length - 1) {
+    array = Array.from(new Set(array.sort()));
+
+    if (start > end) {
+        return null;
+    }
+    let mid = Number.parseInt((start + end) / 2);
+    const node = Node(array[mid]);
+    node.left = buildTree(array, start, mid - 1);
+    node.right = buildTree(array, mid + 1, end);
+
+    return node;
+}
+console.log(JSON.stringify(buildTree([1, 2, 3, 4, 5, 6, 7, 8, 9]), null, 4));
